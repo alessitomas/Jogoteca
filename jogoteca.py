2649ccaae8763d1061e91760846664c4d0bccccd
Alessi181 <tomasalessi@gmail.com>
2022-10-23 14:13:01 -0300

    log in and logout routes

--- jogoteca.py
+++ jogoteca.py
@@ -1,5 +1,6 @@
 from crypt import methods
-from flask import Flask, render_template, request, redirect
+from pickle import NONE
+from flask import Flask, render_template, request, redirect , session, flash
 
 
 class Jogo():
@@ -13,6 +14,7 @@ jogo3 = Jogo('GTAV', 'Simulação', 'XboxONE')
 lista_jogos = [jogo1,jogo2,jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 
 @app.route('/')
@@ -24,8 +26,7 @@ def display():
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
 
-
-@app.route('/criar',methods=['GET', 'POST'])
+@app.route('/criar', methods=['POST'])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
@@ -33,6 +34,29 @@ def criar():
     jogo = Jogo(nome,categoria,console)
     lista_jogos.append(jogo)
     return redirect('/')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar',methods=['POST'])
+def autenficar():
+    if request.form['senha'] == 'toti':
+        session['usuario_logado'] = request.form['usuario']
+        nome_user = session['usuario_logado']
+        flash(f'{nome_user} logado com sucesso')
+        return redirect('/')
+    else:
+        flash('Usuário não foi logado com sucesso')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso')
+    return redirect('/')
+
+
 
 
 
